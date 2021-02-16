77692b2a50ab3b5e40c02d592d6d4245a16859a3
cp3fr <pfeifferchr@gmail.com>
2021-02-16 21:16:57 +0100

    wip

--- analysis/pipeline_performance.py
+++ analysis/pipeline_performance.py
@@ -3,47 +3,51 @@ try:
 except:
     from functions.visattention import *
 
-# Select Input files and folders for processing
+"""
+Processing pipeline for visualizing and quantifying network performance as 
+compared to some reference (human pilot or MPC).
+"""
+
+# Chose input files and folders for processing.
 filepath_track = './tracks/flat.csv'
 filepath_reference = './logs/resnet_test/trajectory_reference_original.csv'
 folderpath = './logs/'
 
-# Collect a list of trajectory logfile paths from a given folderpath.
+# Make a list of input logfiles of network trajectories.
 filepaths = []
 for w in os.walk(folderpath):
     for f in w[2]:
         if (f.find('.csv') > 0) and (filepath_reference.find(f) < 0):
             filepaths.append(os.path.join(w[0], f))
 
-# Extract trajectory, reference, and events from given logfile path.
+# Process individual trajectories flown by the network.
 for filepath in filepaths:
     print('..processing {}'.format(filepath))
-    # Make output path
-    outpath = filepath.replace(
-        '.csv', '/').replace(
-        '/logs/', '/process/').replace(
-        'trajectory_', ''
-    )
+    # Make output folder
+    outpath = filepath.replace('.csv',
+                               '/').replace('/logs/',
+                                            '/process/').replace('trajectory_',
+                                                                 '')
     make_path(outpath)
-    # Copy track, trajectory, reference to output path
+    # Copy track, trajectory, reference files to output folder
     copyfile(filepath,
              outpath + 'trajectory.csv')
     copyfile(filepath_reference,
              outpath + 'reference.csv')
     copyfile(filepath_track,
              outpath + 'track.csv')
-    # Save pass and collision events to output path
+    # Save gate pass and collision events to output folder
     E = get_pass_collision_events(filepath_trajectory=filepath,
                                   filepath_track=filepath_track)
     E.to_csv(outpath + 'events.csv', index=False)
 
-    # Todo: Compute performance features
+    # Todo: Save performance features to output folder
 
-    # Todo: Plot drone state comparison to reference
+    # Todo: Save plot of drone state comparison trajectory vs reference
 
-    # Todo: Plot trajectory with poses
+    # Todo: Save plot trajectory with poses
 
-    # Todo: Optional: Save an animation
+    # Todo: Optional: Save Animation
 
 
 # Todo: Collect features across muliple repetitions
