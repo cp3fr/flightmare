2f4a25ca51df6f54b0feba03b53179e372810595
cp3fr <pfeifferchr@gmail.com>
2021-05-19 14:12:12 +0200

    Code cleanup after Checkpoint class update.

--- analysis/utils.py
+++ analysis/utils.py
@@ -14,183 +14,6 @@ from skspatial.plotting import plot_3d
 from scipy.stats import iqr
 
 
-# class Checkpoint(object):
-#     """
-#     Checkpoint object represented as 2D surface in 3D space with x-axis
-#     pointing in the direction of flight/desired passing direction.
-#
-#     Contains useful methods for determining distance of points,
-#     and intersections with the plane.
-#     """
-#
-#     def __init__(self, df, dims=None, dtype='default'):
-#         #set variable names according to the type of data
-#         if dtype=='gazesim':
-#             position_varnames = ['pos_x', 'pos_y', 'pos_z']
-#             rotation_varnames = ['rot_x_quat', 'rot_y_quat', 'rot_z_quat', 'rot_w_quat']
-#             dimension_varnames = ['dim_y', 'dim_z']
-#             dimension_scaling_factor = 2.5
-#         elif dtype=='liftoff':
-#             position_varnames = ['position_x [m]', 'position_y [m]', 'position_z [m]']
-#             rotation_varnames = [ 'rotation_x [quaternion]', 'rotation_y [quaternion]', 'rotation_z [quaternion]',
-#                                   'rotation_w [quaternion]']
-#             dimension_varnames = ['checkpoint-size_y [m]', 'checkpoint-size_z [m]']
-#             dimension_scaling_factor = 1.
-#         else: #default
-#             position_varnames = ['px', 'py', 'pz']
-#             rotation_varnames = ['qx', 'qy', 'qz', 'qw']
-#             dimension_varnames = ['dy', 'dz']
-#             dimension_scaling_factor = 1.
-#         #current gate center position
-#         p = df[position_varnames].values.flatten()
-#         #current gate rotation quaterion
-#         q = df[rotation_varnames].values.flatten()
-#         #if no width and height dimensions were specified
-#         if dims is None:
-#             dims = (df[dimension_varnames[0]] * dimension_scaling_factor, df[dimension_varnames[1]] * dimension_scaling_factor)
-#         #half width and height of the gate
-#         hw = dims[0] / 2. #half width
-#         hh = dims[1] / 2. #half height
-#         #assuming gates are oriented in the direction of flight: x=forward, y=left, z=up
-#         proto = np.array([[ 0.,  0.,  0.,  0.,  0.],  # assume surface, thus no thickness along x axis
-#                           [ hw, -hw, -hw,  hw,  hw],
-#                           [ hh,  hh, -hh, -hh,  hh]])
-#         self._corners = (Rotation.from_quat(q).apply(proto.T).T + p.reshape(3, 1)).astype(float)
-#         self._center = p
-#         self._rotation = q
-#         self._normal = Rotation.from_quat(q).apply(np.array([1, 0, 0]))
-#         self._width = dims[0]
-#         self._height = dims[1]
-#         #1D minmax values
-#         self._x = np.array([np.min(self._corners[0, :]), np.max(self._corners[0, :])])
-#         self._y = np.array([np.min(self._corners[1, :]), np.max(self._corners[1, :])])
-#         self._z = np.array([np.min(self._corners[2, :]), np.max(self._corners[2, :])])
-#         #2D line representations of gate horizontal axis
-#         self._xy = LineString([ ((np.min(self._corners[0, :])), np.min(self._corners[1, :])),
-#                                 ((np.max(self._corners[0, :])), np.max(self._corners[1, :]))])
-#         self._xz = LineString([((np.min(self._corners[0, :])), np.min(self._corners[2, :])),
-#                                ((np.max(self._corners[0, :])), np.max(self._corners[2, :]))])
-#         self._yz = LineString([((np.min(self._corners[1, :])), np.min(self._corners[2, :])),
-#                                ((np.max(self._corners[1, :])), np.max(self._corners[2, :]))])
-#         #plane representation
-#         center_point = Point(list(self._center))
-#         normal_point = Point(list(self._center + self._normal))
-#         normal_vector = Vector.from_points(center_point, normal_point)
-#         self.plane = Plane(point=center_point, normal=normal_vector)
-#
-#         self.x_axis = Line(point=self._corners[:, 0],
-#                       direction=self._corners[:, 1] - self._corners[:, 0])
-#         self.y_axis = Line(point=self._corners[:, 0],
-#                       direction=self._corners[:, 3] - self._corners[:, 0])
-#         self.length_x_axis = self.x_axis.direction.norm()
-#         self.length_y_axis = self.y_axis.direction.norm()
-#
-#     @property
-#     def width(self):
-#         return self._width
-#
-#     @property
-#     def height(self):
-#         return self._height
-#
-#     @property
-#     def corners(self):
-#         return self._corners
-#
-#     @property
-#     def center(self):
-#         return self._center
-#
-#     @property
-#     def rotation(self):
-#         return self._rotation
-#
-#     @property
-#     def x(self):
-#         return self._x
-#
-#     @property
-#     def y(self):
-#         return self._y
-#
-#     @property
-#     def z(self):
-#         return self._z
-#
-#     @property
-#     def xy(self):
-#         return self._xy
-#
-#     @property
-#     def xz(self):
-#         return self._xz
-#
-#     @property
-#     def yz(self):
-#         return self._yz
-#
-#     def get_distance(self, p):
-#         return self.plane.distance_point(p)
-#
-#     def get_signed_distance(self, p):
-#         return self.plane.distance_point_signed(p)
-#
-#     def intersect(self, p0: np.ndarray([]), p1: np.ndarray([])) -> tuple():
-#         """
-#         Returns 2D and 3D intersection points with the gate surface and a
-#         line from two given points (p0, p1).
-#         """
-#         # Initialize relevant variables
-#         point_2d = None
-#         point_3d = None
-#         _x = None
-#         _y = None
-#         _z = None
-#         count = 0
-#         #only proceed if no nan values
-#         if (np.sum(np.isnan(p0).astype(int))==0) & (np.sum(np.isnan(p1).astype(int))==0):
-#             #line between start end end points
-#             line_xy = LineString([(p0[0], p0[1]),
-#                                   (p1[0], p1[1])])
-#             line_xz = LineString([(p0[0], p0[2]),
-#                                   (p1[0], p1[2])])
-#             line_yz = LineString([(p0[1], p0[2]),
-#                                   (p1[1], p1[2])])
-#             if self.xy.intersects(line_xy):
-#                 count += 1
-#                 _x, _y = [val for val in self.xy.intersection(line_xy).coords][0]
-#             if self.xz.intersects(line_xz):
-#                 count += 1
-#                 _x, _z = [val for val in self.xz.intersection(line_xz).coords][0]
-#             if self.yz.intersects(line_yz):
-#                 count += 1
-#                 _y, _z = [val for val in self.yz.intersection(line_yz).coords][0]
-#             #at least two of the three orthogonal lines need to be crossed
-#             if count > 1:
-#                 point_3d = np.array([_x, _y, _z])
-#                 point_2d = self.point2d(point_3d)
-#         return point_2d, point_3d
-#
-#     def point2d(self, p: np.ndarray([])) -> np.ndarray([]):
-#         """
-#         Returns normalized [0-1 range] 2D coordinates of the intersection
-#         point within gate surface.
-#
-#         The origin is the upper left corner (1st corner point)
-#         X-axis is to the right
-#         Y-axis is down
-#         """
-#         # Project the 3D intersection point onto the x and y surface axies.
-#         px_projected = self.x_axis.project_point(p)
-#         py_projected = self.y_axis.project_point(p)
-#         length_px_projected = self.x_axis.point.distance_point(px_projected)
-#         length_py_projected = self.y_axis.point.distance_point(py_projected)
-#         # Return the normalized 2D projection of the intersection point.
-#         return np.array([length_px_projected / self.length_x_axis,
-#                          length_py_projected / self.length_y_axis])
-#
-
-
 class Checkpoint:
     """
     Checkpoint object represented as 2D surface in 3D space with x-axis
@@ -772,64 +595,6 @@ def format_trajectory_figure(
     return ax
 
 
-# def get_pass_collision_events(
-#         filepath_trajectory: str,
-#         filepath_track: str,
-#         gate_inner_dimensions: tuple=(2.5, 2.5),
-#         gate_outer_dimensions: tuple=(3.5, 3.5),
-#         wall_collider_dimensions: tuple=(66, 36, 9),
-#         wall_collider_center: tuple=(0, 0, 4.85)
-#         ) -> pd.DataFrame():
-#     """Returns an events dataframe of pass and collision events from given
-#     trajectory and track filepaths."""
-#     # Load race track information.
-#     T = pd.read_csv(filepath_track)
-#     # Define checkpoints and colliders.
-#     gate_checkpoints = [
-#         Checkpoint(T.iloc[i], dims=gate_inner_dimensions) for i in range(T.shape[0])]
-#     gate_colliders = [
-#         Checkpoint(T.iloc[i], dims=gate_outer_dimensions) for i in range(T.shape[0])]
-#     wall_colliders = get_wall_colliders(dims=wall_collider_dimensions,
-#                                         center=wall_collider_center)
-#     # Load a trajectory
-#     D = pd.read_csv(filepath_trajectory)
-#     t = D['t'].values
-#     px = D['px'].values
-#     py = D['py'].values
-#     pz= D['pz'].values
-#     # Detect checkpoint passing and collision events.
-#     events = {}
-#     for key, objects in [('gate_pass', gate_checkpoints),
-#                          ('gate_collision', gate_colliders),
-#                          ('wall_collision', wall_colliders)
-#                         ]:
-#         for id in range(len(objects)):
-#             object = objects[id]
-#             for timestamp in detect_checkpoint_pass(t, px, py, pz, object):
-#                 if not ((key == 'gate_collision') and (
-#                         timestamp in events.keys())):
-#                     events[timestamp] = (key, id)
-#     # Make events dataframe
-#     E = pd.DataFrame({
-#         't': [
-#             k
-#             for k in sorted(events)],
-#         'object-id': [
-#             events[k][1]
-#             for k in sorted(events)],
-#         'object-name': [
-#             events[k][0].split('_')[0]
-#             for k in sorted(events)],
-#         'is-collision': [
-#             int(events[k][0].find('collision') > 0)
-#             for k in sorted(events)],
-#         'is-pass':  [
-#             int(events[k][0].find('pass') > 0)
-#             for k in sorted(events)]},
-#         index=list(range(len(events)))
-#         )
-#     return E
-
 def get_pass_collision_events(
         filepath_trajectory: str,
         filepath_track: str,
@@ -852,9 +617,6 @@ def get_pass_collision_events(
     # Load a trajectory
     D = pd.read_csv(filepath_trajectory)
     t = D['t'].values
-    # px = D['px'].values
-    # py = D['py'].values
-    # pz= D['pz'].values
     p = D.loc[:, ('px', 'py', 'pz')].values
     # Detect checkpoint passing and collision events.
     events = {}
@@ -864,7 +626,6 @@ def get_pass_collision_events(
                         ]:
         for id in range(len(objects)):
             object = objects[id]
-            # for timestamp in detect_checkpoint_pass(t, px, py, pz, object):
             for timestamp in object.detect_pass(t=t, p=p):
                 if not ((key == 'gate_collision') and (
                         timestamp in events.keys())):
